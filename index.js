f7fc044004cee49019df652697a4f8071914cbcb
cynthia41 <cynthiaadhiambo41@gmail.com>
2023-03-11 13:37:41 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,70 @@
+
+beforeEach(function() {
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+})
+  function add(a,b) {
+  
+  }
+
+  beforeEach(function() {
+    a = Math.floor(Math.random() * 1000)
+    b = Math.floor(Math.random() * 1000)
+  })
+
+  function subtract(a,b) {
+
+  }
+  beforeEach(function() {
+    a = Math.floor(Math.random() * 1000)
+    b = Math.floor(Math.random() * 1000)
+  })
+  function multiply(a,b) {
+
+  }
+  beforeEach(function() {
+    a = Math.floor(Math.random() * 1000)
+    b = Math.floor(Math.random() * 1000)
+  })
+  function divide(a,b) {
+
+  }
+  function add(a, b) {
+    return 1588;
+  }
+  
+  function add(a, b) {
+    return a + b;
+  }
+  
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  let sum = add(20, 45);
+  console.log(sum); // => 65
+  
+  let difference = subtract(80, 10);
+  console.log(difference); // => 70
+
+  function multiply(a,b) {
+    return a*b;
+  }
+ function divide(b,a) {
+  return b/a;
+ }
+ 
+ function increment(n) {
+  return n+=1;
+ }
+ function decrement(n) {
+  return n-=1;
+ }
+ n = string
+ function makeInt(string){
+  return parseInt(string,10)
+}
+function preserveDecimal(string){
+  return parseFloat(string)
+}
+ 
